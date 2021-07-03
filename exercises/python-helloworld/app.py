ec635609e373cb9baa2d3c76d8d9d073371539c1
jhon <supersharas@gmail.com.com>
2021-07-03 05:29:04 +0100

    status and metrics

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,3 +1,6 @@
+
+import json
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -7,3 +10,13 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
+@app.route("/status")
+def status():
+    return json.dumps({'user':'admin', 'result':'OK - healthy'})
+
+@app.route("/metrics")
+def metrics():
+    user_count = 140
+    user_count_active = 60
+    return json.dumps({'user':'admin', 'data':{"UserCount": user_count, "UserCountActive": user_count_active}})
